2373dc08efc24fa26a9f29ab8b8eef4aa8d3c340
Jonathan Arnold <jon@mixnblend.com>
2017-11-06 21:02:19 +0200

    Added line to copy ports example file to ports.conf and removed ports.conf from repo

--- copyfiles.sh
+++ copyfiles.sh
@@ -12,6 +12,12 @@ cp ./docker-config-files/apache/apache2.conf.example ./docker-config-files/apach
 
 echo "...done"
 
+echo "Copying example ports.conf file to docker-config-files/apache/ports.conf \c"
+
+cp ./docker-config-files/apache/ports.conf.example ./docker-config-files/apache/ports.conf
+
+echo "...done"
+
 echo "Copying example php.ini file to docker-config-files/php/php.ini \c"
 
 cp ./docker-config-files/php/php.ini.example ./docker-config-files/php/php.ini
